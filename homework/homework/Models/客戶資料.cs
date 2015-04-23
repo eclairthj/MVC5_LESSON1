97d007e1c1fc5e5f7c371ca00b8b0262442de572
eclair <eclair.thj@gmail.com>
2015-04-23 14:10:05 +0800

    Required

--- homework/homework/Models/客戶資料.cs
+++ homework/homework/Models/客戶資料.cs
@@ -11,7 +11,8 @@ namespace homework.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class 客戶資料
     {
         public 客戶資料()
@@ -19,15 +20,21 @@ namespace homework.Models
             this.客戶銀行資訊 = new HashSet<客戶銀行資訊>();
             this.客戶聯絡人 = new HashSet<客戶聯絡人>();
         }
-    
+
         public int Id { get; set; }
+        [Required]
         public string 客戶名稱 { get; set; }
+        [Required]
         public string 統一編號 { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "PhoneNumber is not valid")]
         public string 電話 { get; set; }
         public string 傳真 { get; set; }
         public string 地址 { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
-    
+
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual ICollection<客戶聯絡人> 客戶聯絡人 { get; set; }
     }
