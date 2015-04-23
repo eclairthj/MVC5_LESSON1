7b11339edd3d805e3c91a5c57ceea7401edcb74f
eclair <eclair.thj@gmail.com>
2015-04-23 13:54:08 +0800

    Add Report and View

---
+++ homework/homework/Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace homework.Controllers
+{
+    public class ReportController : Controller
+    {
+        客戶資料Entities db = new 客戶資料Entities();
+        // GET: Report
+        public ActionResult Index()
+        {
+            IQueryable<客戶總表> vm = db.客戶總表.AsQueryable();
+
+            return View(vm);
+        }
+    }
+}
---
+++ homework/homework/Models/ReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace homework.Models
+{
+    public class ReportViewModel
+    {
+        public string 客戶名稱 { get; set; }
+        public int 聯絡人數量 { get; set; }
+        public int 銀行帳戶數量 { get; set; }
+    }
+}
